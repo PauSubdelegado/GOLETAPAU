94f2bb33bc69740aef59df02df6917ea71b81087
PauSubdelegado <paumorant19@gmail.com>
2026-02-19 14:37:54 +0100

    parte 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,6 @@ const Header = () => {
             <div className='col-12 fondo px-0'>
                 <div className='fondoH d-flex justify-content-between align-items-center px-4'>
                     <img src={restarunteLogo} alt="Restaurante" className="d-flex" id='logo' width={230} />
-                    <p>Tu sitio de Confianza</p>
                     <button className='btn  reserva fs-5'><i className="bi bi-journal-text pe-2"></i>Reservar</button>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,87 +21,66 @@ const home = () => {
             satisfacer a nuestros comensales.
           </p>
         </div>
-        <div className="col-12">
-          <div className="d-flex justify-content-center align-items-center">
-            <div id="carouselExampleIndicators" class="carousel slide w-50">
-              <div class="carousel-indicators">
+        <div className="col-4 px-0"></div>
+        <div className="col-4 px-0">
+            <div className="">
+              <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src={img2} class="d-block rotar" alt="Carne Ahumada" />
+                  </div>
+                  <div class="carousel-item">
+                    <img src={img2} class="d-block rotar" alt="Paella 2" />
+                  </div>
+                  <div class="carousel-item">
+                    <img src={img2} class="d-block rotar" alt="Paella 1" />
+                  </div>
+                </div>
                 <button
+                  class="carousel-control-prev"
                   type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="0"
-                  class="active"
-                  aria-current="true"
-                  aria-label="Slide 1"
-                ></button>
+                  data-bs-target="#carouselExample"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
                 <button
+                  class="carousel-control-next"
                   type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="1"
-                  aria-label="Slide 2"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="2"
-                  aria-label="Slide 3"
-                ></button>
+                  data-bs-target="#carouselExample"
+                  data-bs-slide="next"
+                >
+                  <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
               </div>
-
-              <div class="carousel-inner">
-                <div class="carousel-item active w-auto">
-                  <img src={img1} class="d-block w-50" alt="Logo Goleta" />
-                </div>
-                <div class="carousel-item w-auto">
-                  <img src={img2} class="d-block w-50" alt="Paella 2" />
-                </div>
-                <div class="carousel-item w-auto">
-                  <img src={img3} class="d-block w-50" alt="Paella 1" />
-                </div>
-              </div>
-
-              <button
-                class="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev"
-              >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Anterior</span>
-              </button>
-              <button
-                class="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next"
-              >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Siguiente</span>
-              </button>
             </div>
-          </div>
         </div>
-        <div className="col-2 text-center mt-5 align-items-center align-content-center">
+        <div className="col-4 px-0"></div>
+        <div className="col-12 col-md-2 text-center mt-5 align-items-center align-content-center">
           <p className="fs-4 fw-bold">VEN A COMER</p>
           <p>Carrer de la Mar Mediterrània, 7, 46780 Oliva, Valencia</p>
+          <p>625 48 04 38</p>
         </div>
-        <div className="col-2 text-center mt-5 align-content-center">
+        <div className="col-12 col-md-2 text-center mt-5 align-content-center">
           <p className="fs-4 fw-bold">HORARIO</p>
           <p>
             De lunes a domingo* 9:00h a 00:00h * Martes: cerrado por descanso
             del personal Cocina de 9:00h a 23:00h
           </p>
         </div>
-        <div className="col-2 text-center mt-5 align-content-center">
-          <p className="fs-4 fw-bold">VEN A COMER</p>
-          <p>Carrer de la Mar Mediterrània, 7, 46780 Oliva, Valencia</p>
+        <div className="col-12 col-md-2 text-center mt-5 align-content-center">
+          <p className="fs-4 fw-bold">PIDE TU ARROZ</p>
+          <p>Para pedir tu arroz, reservalo antes de las 12:30h</p>
         </div>
-        <div className="col-5 mt-5">
+        <div className="col-12 col-md-5 mt-5">
           <img src={img4} className="img-fluid rounded" alt="..."></img>
         </div>
       </div>
