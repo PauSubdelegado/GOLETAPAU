cad6b898129eeffd1aad1356e1955f0d526eaf6a
PauSubdelegado <paumorant19@gmail.com>
2026-02-24 13:56:33 +0100

    nose

--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import ModalPlatos from "./ModalPlatos";
 
 function Carta() {
   const [platos, setPlatos] = useState([]);
@@ -7,6 +8,7 @@ function Carta() {
   const principales = platos.filter((plato) => plato.categoria === "principal");
   const postres = platos.filter((plato) => plato.categoria === "postre");
   const bebidas = platos.filter((plato) => plato.categoria === "bebida");
+  const [platoSeleccionado, setPlatoSeleccionado] = useState(null);
 
   useEffect(() => {
     axios
@@ -17,40 +19,54 @@ function Carta() {
 
   return (
     <>
-    <div className="row">
+      <div className="row">
         <div className="fondoH">
-        <p className="fs-3 text-center my-3">ENTRANTES</p>
+          <p className="fs-3 text-center my-3">ENTRANTES</p>
         </div>
-        {entrantes.map(plato => (
-        <div className=" col-12 col-md-6 my-4 text-center" key={plato.id}>
-            <div className="h-100 d-flex flex-column p-3 text-center mx-auto "style={{ maxWidth: "500px" }}>
-          <h5>{plato.nombre}</h5>
-          <p>{plato.descripcion}</p>
-          <p className="">
-            {Number.isInteger(Number(plato.precio))
-              ? Number(plato.precio)
-              : Number(plato.precio).toFixed(2)}
-          </p>
+        {entrantes.map((plato) => (
+          <div className=" col-12 col-md-6 my-4 text-center" key={plato.id}>
+            <div
+              className="h-100 d-flex flex-column p-3 text-center mx-auto "
+              style={{ maxWidth: "500px" }}
+            >
+              <h5>{plato.nombre}</h5>
+              <p>{plato.descripcion}</p>
+              <p className="">
+                {Number.isInteger(Number(plato.precio))
+                  ? Number(plato.precio)
+                  : Number(plato.precio).toFixed(2)}
+              </p>
+              <button onClick={() => setPlatoSeleccionado(plato)}>ℹ️</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-    <div className="row">
+        ))}
+       
+      </div>
+       {platoSeleccionado && (
+          <ModalPlatos
+            plato={platoSeleccionado}
+            onClose={() => setPlatoSeleccionado(null)}
+          />
+        )}
+      <div className="row">
         <div className="fondoH">
-        <h3 className="text-center my-3">Principales</h3>
+          <h3 className="text-center my-3">Principales</h3>
         </div>
-        {principales.map(plato => (
-        <div className=" col-12 col-md-4 my-4 mx-4 text-center" key={plato.id}>
-          <h5>{plato.nombre}</h5>
-          <p className="">{plato.descripcion}</p>
-          <p className="">
-            {Number.isInteger(Number(plato.precio))
-              ? Number(plato.precio)
-              : Number(plato.precio).toFixed(2)}
-          </p>
-        </div>
-      ))}
-    </div>
+        {principales.map((plato) => (
+          <div
+            className=" col-12 col-md-4 my-4 mx-4 text-center"
+            key={plato.id}
+          >
+            <h5>{plato.nombre}</h5>
+            <p className="">{plato.descripcion}</p>
+            <p className="">
+              {Number.isInteger(Number(plato.precio))
+                ? Number(plato.precio)
+                : Number(plato.precio).toFixed(2)}
+            </p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
---
+++ src/pages/ModalPlatos.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function ModalPlatos({ plato, onClose }) {
+  if (!plato) return null;
+
+  return (
+    <>
+      {/* Fons semi-transparent que bloqueja la resta de la pàgina */}
+      <div 
+        className="modal-backdrop fade show"
+        style={{ zIndex: 1040 }}
+      ></div>
+
+      {/* Modal centrat */}
+      <div
+        className="modal d-block"
+        tabIndex="-1"
+        style={{ zIndex: 1050 }}
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{plato.nombre}</h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={onClose}
+              ></button>
+            </div>
+            <div className="modal-body text-center">
+              <p>{plato.descripcion}</p>
+              {plato.imagen && (
+                <img
+                  src={"/fotosComida/paella2.jpg"} 
+                  alt={plato.nombre}
+                  className="img-fluid"
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ModalPlatos;
